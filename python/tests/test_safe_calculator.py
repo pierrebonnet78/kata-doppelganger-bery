d0e24c7c2ad6cadfd401f03df4aefea1342eec67
Pierre Bonnet <pierre.bonnet.19@efrei.net>
2025-01-08 16:18:22 +0100

    1.SafeCalculator Part 2

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,16 +1,23 @@
 import pytest
 from safe_calculator import SafeCalculator
 from tests.authorizer import Authorizer
+from unittest.mock import Mock
 
 def test_divide_should_not_raise_any_error_when_authorized():
-    # TODO: write a test that fails due to the bug in
-    # SafeCalculator.add
     authorizer = Authorizer()
     authorizer.set_authorized(True)
 
     calculator = SafeCalculator(authorizer)
-
-
     result = calculator.add(1, 1)
+
+    assert result == 2, "Result should be 2"
+
+def test_divide_with_mock_authorizer():
+    mock_authorizer = Mock()
+    mock_authorizer.is_authorized.return_value = True
+
+    calculator = SafeCalculator(mock_authorizer)
+    result = calculator.add(1, 1)
+    
     assert result == 2, "Result should be 2"
     
